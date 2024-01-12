0dc48b7735427108e94aee91df3119a5c7c56399
donuthappy <mateo.richard420@gmail.com>
2024-01-12 08:32:30 -0800

    update_240112

--- client/src/services/auth-services.tsx
+++ client/src/services/auth-services.tsx
@@ -1,27 +1,27 @@
 import jwtAuthAxios from "./auth/jwtAuth";
 
-const authServices: {
-    getCurrentUser: () => Promise<string | null>;
-    signIn: (loginCreds: { email: string; password: string }) => Promise<any>;
-  } = {
-      getCurrentUser: function (): Promise<string | null> {
-        //   throw new Error("Function not implemented.");
-          throw new Error("Function not implemented.");
-      },
-      signIn: function (loginCreds: { email: string; password: string; }): Promise<any> {
-          throw new Error("Function not implemented.");
-      }
-  };
+// const authServices: {
+//     getCurrentUser: () => Promise<string | null>;
+//     signIn: (loginCreds: { email: string; password: string }) => Promise<any>;
+//   } = {
+//       getCurrentUser: function (): Promise<string | null> {
+//         //   throw new Error("Function not implemented.");
+//           throw new Error("Function not implemented.");
+//       },
+//       signIn: function (loginCreds: { email: string; password: string; }): Promise<any> {
+//           throw new Error("Function not implemented.");
+//       }
+//   };
 
-authServices.getCurrentUser = async () => {
-    const user = localStorage.getItem("user");
-    return user;
-};
+// authServices.getCurrentUser = async () => {
+//     const user = localStorage.getItem("user");
+//     return user;
+// };
 
-//loginCreds must be {email: "abc@example.com", password: "ABC123DEF"}
-authServices.signIn = async (loginCreds) => {
-    const {data} = await jwtAuthAxios.post('/auth', loginCreds);
-    return data;
-};
+// //loginCreds must be {email: "abc@example.com", password: "ABC123DEF"}
+// authServices.signIn = async (loginCreds) => {
+//     const {data} = await jwtAuthAxios.post('/auth', loginCreds);
+//     return data;
+// };
 
-export default authServices;
+// export default authServices;
